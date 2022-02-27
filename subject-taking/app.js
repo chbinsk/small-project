b4b40075270d35da1bec2782269d9227cf6351af
eugeniusms <eugeniusms@gmail.com>
2022-02-27 20:05:30 +0700

    Run My App

--- subject-taking/app.js
+++ subject-taking/app.js
@@ -77,6 +77,40 @@ const subjectPrequisite = {
     ]
 }
 
+const allSubject = [
+    "Dasar-Dasar Pemrograman 1",
+    "Pengantar Sistem Digital",
+    "Matematika Diskret 1",
+    "Kalkulus 1",
+    // term 2
+    "Dasar-Dasar Pemrograman 2",
+    "Pengantar Organisasi Komputer",
+    "Matematika Diskret 2",
+    "Kalkulus 2",
+    // term 3
+    "Pemrograman Berbasis Platform",
+    "Struktur Data dan Algoritma",
+    "Metodologi Penelitian dan Penulisan Ilmiah",
+    "Sistem Operasi",
+    "Aljabar Linier",
+    // term 4
+    "Statistika dan Probabilitas",
+    "Sistem Interaksi",
+    "Pemrograman Lanjut",
+    "Teori Bahasa dan Automata",
+    "Basis Data",
+    // term 5
+    "Jaringan Komputer",
+    "Kecerdasan Artifisial dan Sains Data Dasar",
+    "Analisis Numerik",
+    "Desain dan Analisis Algoritma",
+    "Rekayasa Perangkat Lunak",
+    // term 6-8
+    "Proyek Perangkat Lunak",
+    "Komputer dan Masyarakat",
+    "Tugas Akhir"
+]
+
 function len(array) {
     return array.length
 }
@@ -94,6 +128,87 @@ function recursive(object, search, array) {
     }
 }
 
+function searchSubject(search) {
+    // search by sequences
+    console.log(search)
+    const result = []
+    const searchSub = search.toLowerCase()
+    const searchLength = searchSub.length
+    for (matkul of allSubject) {
+        const mataKuliah = matkul.toLowerCase()
+        if (searchSub == mataKuliah.slice(0, searchLength)) {
+            result.push(matkul)
+        }
+    }
+    return result
+}
+
+const subject= document.querySelector("#subject")
+const board = document.querySelector("#board")
+const searchButton = document.querySelector("#searchButton")
+const printList = document.querySelector("#subjectList")
+
 let myArray = []
-recursive(subjectPrequisite, "Proyek Perangkat Lunak", myArray)
+// recursive(subjectPrequisite, subject, myArray)
 console.log(myArray)
+
+// tombol pilih matkul search
+setInterval(() => {
+    const subjectSearchButton = document.querySelectorAll('button');
+    for (let buton of subjectSearchButton) {
+        if (buton.className == "button is-light") {
+            console.log(buton)
+            buton.addEventListener('click', function () {
+                console.log(this.id)
+                this.className = "button is-success"
+                printMataKuliah(this.id)
+            });
+        }
+    }
+}, 1000)
+
+subject.addEventListener('input', function (e) {
+    board.innerText = subject.value;
+    console.log(searchSubject(subject.value))
+
+    const searchSubjectList = searchSubject(subject.value)
+    const searchListHTML = document.querySelector("#searchList")
+    // clear search first every ngetik wkwk
+    searchListHTML.innerText = ""
+
+    for (s of searchSubjectList) {
+        const newChoice = document.createElement("button")
+        newChoice.className = "button is-light"
+        newChoice.id = s
+        newChoice.innerText = s;
+        searchListHTML.append(newChoice);
+    }
+});
+
+// fungsi print daftar mata kuliah prerequisite
+function printMataKuliah(search) {
+    let myArray = []
+    recursive(subjectPrequisite, search, myArray)
+    console.log(myArray)
+
+    // reset isi printList
+    printList.innerText = ""
+    const need = document.querySelector("#need")
+    need.innerText = "Mata kuliah membutuhkan kelulusan :"
+
+    for (i = 0; i < myArray.length; i++) {
+        const oneSubject = document.createElement("ul")
+        oneSubject.innerText = myArray[i]
+        printList.append(oneSubject)
+    }
+}
+
+searchButton.addEventListener('click', function (e) {
+    console.log(subject.value);
+    printMataKuliah(subject.value)
+})
+
+
+
+
+// bikin fitur search memudahkan click
