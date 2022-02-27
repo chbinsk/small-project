ad2ecc3f3a37c805fc38296262d41515a1ff672e
eugeniusms <eugeniusms@gmail.com>
2022-02-27 17:23:48 +0700

    Add Subject Taking

---
+++ subject-taking/app.js
@@ -0,0 +1,96 @@
+const subjectPrequisite = {
+    // term 1
+    "Dasar-Dasar Pemrograman 1": [],
+    "Pengantar Sistem Digital": [],
+    "Matematika Diskret 1": [],
+    "Kalkulus 1": [],
+    // term 2
+    "Dasar-Dasar Pemrograman 2": [
+        "Dasar-Dasar Pemrograman 1"
+    ],
+    "Pengantar Organisasi Komputer": [
+        "Pengantar Sistem Digital"
+    ],
+    "Matematika Diskret 2": [],
+    "Kalkulus 2": [
+        "Kalkulus 1"
+    ],
+    // term 3
+    "Pemrograman Berbasis Platform": [
+        "Dasar-Dasar Pemrograman 1"
+    ],
+    "Struktur Data dan Algoritma": [
+        "Dasar-Dasar Pemrograman 1"
+    ],
+    "Metodologi Penelitian dan Penulisan Ilmiah": [],
+    "Sistem Operasi": [
+        "Pengantar Organisasi Komputer"
+    ],
+    "Aljabar Linier": [],
+    // term 4
+    "Statistika dan Probabilitas": [
+        "Matematika Diskret 1"
+    ],
+    "Sistem Interaksi": [
+        "Pemrograman Berbasis Platform"
+    ],
+    "Pemrograman Lanjut": [
+        "Pemrograman Berbasis Platform",
+        "Dasar-Dasar Pemrograman 2"
+    ],
+    "Teori Bahasa dan Automata": [
+        "Matematika Diskret 1",
+        "Matematika Diskret 2"
+    ],
+    "Basis Data": [
+        "Dasar-Dasar Pemrograman 2"
+    ],
+    // term 5
+    "Jaringan Komputer": [
+        "Sistem Operasi",
+        "Dasar-Dasar Pemrograman 1"
+    ],
+    "Kecerdasan Artifisial dan Sains Data Dasar": [
+        "Aljabar Linier",
+        "Statistika dan Probabilitas"
+    ],
+    "Analisis Numerik": [
+        "Aljabar Linier",
+        "Kalkulus 2"
+    ],
+    "Desain dan Analisis Algoritma": [
+        "Struktur Data dan Algoritma"
+    ],
+    "Rekayasa Perangkat Lunak": [
+        "Dasar-Dasar Pemrograman 2"
+    ],
+    // term 6-8
+    "Proyek Perangkat Lunak": [
+        "Basis Data",
+        "Rekayasa Perangkat Lunak"
+    ],
+    "Komputer dan Masyarakat": [
+        "100 SKS"
+    ],
+    "Tugas Akhir": [
+        "114 SKS"
+    ]
+}
+
+function len(array) {
+    return array.length
+}
+
+function recursive(object, search) {
+    if (len(object[search]) == 0) {
+        console.log([])
+    } else {
+        console.log(object[search])
+        for (i = 0; i < len(object[search]); i++) {
+            recursive(object, object[search][i])
+        }
+    }
+}
+
+
+recursive(subjectPrequisite, "Kecerdasan Artifisial dan Sains Data Dasar")
