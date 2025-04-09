e7947ec466c584563ff0d57c78b40a0b1114c158
chbinsk <ruthchelsea11@gmail.com>
2025-04-09 16:37:45 +0700

    Update app.js

--- subject-taking/app.js
+++ subject-taking/app.js
@@ -1,81 +1,28 @@
 // Object mata kuliah dengan pre-requisitenya
 const subjectPrequisite = {
     // term 1
-    "Dasar-Dasar Pemrograman 1": [],
-    "Pengantar Sistem Digital": [],
-    "Matematika Diskret 1": [],
-    "Kalkulus 1": [],
+    "MPK Agama": [],
+    "MPK Bahasa Inggris": [],
+    "Pengantar Ekonomi 1": [],
+    "Matematika Dasar untuk Ekonomi dan Bisnis": [],
+    "Pengantar Bisnis": [],
+    "Teknik Penulisan Akademik dan Pengantar Komunikasi Bisnis": [],
+    "Statistika Ekonomi dan Bisnis": [],
+    "Literasi dalam Bahasa Inggris": [],
+
     // term 2
-    "Dasar-Dasar Pemrograman 2": [
-        "Dasar-Dasar Pemrograman 1"
-    ],
-    "Pengantar Organisasi Komputer": [
-        "Pengantar Sistem Digital"
-    ],
-    "Matematika Diskret 2": [],
-    "Kalkulus 2": [
-        "Kalkulus 1"
-    ],
+    "MPK Terintegrasi": [],
+    "Pengantar Ekonomi 2": [],
+    "Pengantar Manajemen": [],
+    "Pengantar Akuntansi": [],
+    "Matematika Keuangan": [],
+    "Koperasi": [],
+    "Metode Analisis Kasus Akuntansi dan Bisnis (MAKAB)": [],
+
     // term 3
-    "Pemrograman Berbasis Platform": [
-        "Dasar-Dasar Pemrograman 1"
-    ],
-    "Struktur Data dan Algoritma": [
-        "Dasar-Dasar Pemrograman 1"
-    ],
-    "Metodologi Penelitian dan Penulisan Ilmiah": [],
-    "Sistem Operasi": [
-        "Pengantar Organisasi Komputer"
-    ],
-    "Aljabar Linier": [],
-    // term 4
-    "Statistika dan Probabilitas": [
-        "Matematika Diskret 1"
-    ],
-    "Sistem Interaksi": [
-        "Pemrograman Berbasis Platform"
-    ],
-    "Pemrograman Lanjut": [
-        "Pemrograman Berbasis Platform",
-        "Dasar-Dasar Pemrograman 2"
-    ],
-    "Teori Bahasa dan Automata": [
-        "Matematika Diskret 1",
-        "Matematika Diskret 2"
-    ],
-    "Basis Data": [
-        "Dasar-Dasar Pemrograman 2"
-    ],
-    // term 5
-    "Jaringan Komputer": [
-        "Sistem Operasi",
-        "Dasar-Dasar Pemrograman 1"
-    ],
-    "Kecerdasan Artifisial dan Sains Data Dasar": [
-        "Aljabar Linier",
-        "Statistika dan Probabilitas"
-    ],
-    "Analisis Numerik": [
-        "Aljabar Linier",
-        "Kalkulus 2"
-    ],
-    "Desain dan Analisis Algoritma": [
-        "Struktur Data dan Algoritma"
-    ],
-    "Rekayasa Perangkat Lunak": [
-        "Dasar-Dasar Pemrograman 2"
-    ],
-    // term 6-8
-    "Proyek Perangkat Lunak": [
-        "Basis Data",
-        "Rekayasa Perangkat Lunak"
-    ],
-    "Komputer dan Masyarakat": [
-        "100 SKS"
-    ],
-    "Tugas Akhir": [
-        "114 SKS"
-    ]
+    "Akuntansi Biaya": [],
+    "Pengantar Manajemen Operasi": [],
+    "Pengantar Manajemen": [],
 }
 
 // Objek berisi semua mata kuliah yang ada
